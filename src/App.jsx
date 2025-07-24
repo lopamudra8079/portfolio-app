5741635525ad15a95da9e5e7da8106c26004e894
Lucky <lopamudrashoo@gmail.com>
2025-07-24 12:33:48 +0530

    done port

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Experience from "./Components/Experience";
 import Home from "./Components/Home";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 import Projects from "./Components/Projects";
 
 function App() {
